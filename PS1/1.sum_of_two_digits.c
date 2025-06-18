af74462c8f647243eb20a396853be772780ec35f
Mohammed Farshad <imohammedfarshad@gmail.com>
2025-06-18 11:58:39 +0530

    Update Problem Set 1

---
+++ PS1/1.sum_of_two_digits.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int n1,n2,sum;
+
+    printf("Enter 2 numbers \n");
+    scanf("%d %d", &n1 ,&n2);
+    sum = n1 + n2;
+    printf("%d + %d = %d \n",n1 , n2,sum);
+}
